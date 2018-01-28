327d77de3aab6b515949764fc5e1ca22d4f5cc61
Nasrul Hazim <nasrulhazim.m@gmail.com>
2018-01-28 15:41:21 +0800

    Fixed namespace

--- src/stubs/tests/TestCase.php
+++ src/stubs/tests/TestCase.php
@@ -7,7 +7,7 @@ class TestCase extends \Orchestra\Testbench\TestCase
     protected function getPackageProviders($app)
     {
         return [
-            DummyNamespace\DummyClassNameServiceProvider::class,
+            \DummyNamespace\DummyClassNameServiceProvider::class,
         ];
     }
 }
