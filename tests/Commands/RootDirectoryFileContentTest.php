a7c070bbfa1726ae721413aed4bffb0039467b09
Nasrul Hazim <nasrulhazim.m@gmail.com>
2018-01-28 16:29:32 +0800

    Update root directory file content test

--- tests/Commands/RootDirectoryFileContentTest.php
+++ tests/Commands/RootDirectoryFileContentTest.php
@@ -15,6 +15,7 @@ class RootDirectoryFileContentTest extends SkeletonCommandTest
         $composerJsonContent = "{
     \"name\": \"cleanique-coders/my-console\",
     \"description\": \"Built with Laravel Standalone Package Creator\",
+    \"keywords\": [\"package\", \"laravel\"],
     \"license\": \"MIT\",
     \"authors\": [
         {
@@ -36,6 +37,7 @@ class RootDirectoryFileContentTest extends SkeletonCommandTest
         }
     },
     \"require\": {
+        \"php\": \">=7.0.0\",
         \"illuminate/support\": \"^5.5\"
     },
     \"require-dev\": {
@@ -57,7 +59,10 @@ class RootDirectoryFileContentTest extends SkeletonCommandTest
     {
         $this->assertFileExists($this->package_path . '/README.md');
 
-        $readmeMdContent = "## About Your Package
+        $readmeMdContent = "
+[![Build Status](https://travis-ci.org/cleanique-coders/my-console.svg?branch=master)](https://travis-ci.org/cleanique-coders/my-console) [![Latest Stable Version](https://poser.pugx.org/cleanique-coders/my-console/v/stable)](https://packagist.org/packages/cleanique-coders/my-console) [![Total Downloads](https://poser.pugx.org/cleanique-coders/my-console/downloads)](https://packagist.org/packages/cleanique-coders/my-console) [![License](https://poser.pugx.org/cleanique-coders/my-console/license)](https://packagist.org/packages/cleanique-coders/my-console)
+
+## About Your Package
 
 Tell people about your package
 
@@ -66,7 +71,7 @@ Tell people about your package
 1. In order to install `cleanique-coders/my-console` in your Laravel project, just run the *composer require* command from your terminal:
 
 ```
-composer require cleanique-coders/my-console
+$ composer require cleanique-coders/my-console
 ```
 
 2. Then in your `config/app.php` add the following to the providers array:
@@ -82,6 +87,26 @@ CleaniqueCoders\MyConsole\MyConsoleServiceProvider::class,
 ```
 
 ## Usage
+
+## Test
+
+To run the test, type `vendor/bin/phpunit` in your terminal.
+
+To have codes coverage, please ensure to install PHP XDebug then run the following command:
+
+```
+$ vendor/bin/phpunit -v --coverage-text --colors=never --stderr
+```
+
+## Contributions
+
+Everyone are welcome to contribute to this package. However, it's a good practice to provide:
+
+1. The problem you solved
+2. Provide test
+3. Documentation
+
+Without these 3, you may add extra work for the maintainer.
 
 ## License
 
