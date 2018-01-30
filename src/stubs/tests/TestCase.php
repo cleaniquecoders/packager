ca92711d0400d86661859093118e632491e7af38
Nasrul Hazim <nasrulhazim.m@gmail.com>
2018-01-31 07:52:17 +0800

    Added setUp and getEnvironmentSetup in TestCase

--- src/stubs/tests/TestCase.php
+++ src/stubs/tests/TestCase.php
@@ -4,10 +4,44 @@ namespace DummyNamespace\Tests;
 
 class TestCase extends \Orchestra\Testbench\TestCase
 {
+    /**
+     * Setup the test environment.
+     */
+    public function setUp()
+    {
+        parent::setUp();
+
+        // $this->loadLaravelMigrations(['--database' => 'testbench']);
+
+        // $this->artisan('migrate', ['--database' => 'testbench']);
+    }
+
+    /**
+     * Load Package Service Provider
+     * @param  \Illuminate\Foundation\Application $app
+     * @return array      List of Service Provider
+     */
     protected function getPackageProviders($app)
     {
         return [
             \DummyNamespace\DummyClassNameServiceProvider::class,
         ];
+    }
+
+    /**
+     * Define environment setup.
+     *
+     * @param  \Illuminate\Foundation\Application  $app
+     * @return void
+     */
+    protected function getEnvironmentSetUp($app)
+    {
+        // Setup default database to use sqlite :memory:
+        $app['config']->set('database.default', 'testbench');
+        $app['config']->set('database.connections.testbench', [
+            'driver'   => 'sqlite',
+            'database' => ':memory:',
+            'prefix'   => '',
+        ]);
     }
 }
