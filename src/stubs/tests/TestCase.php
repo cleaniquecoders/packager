201ac6102584bad5a51e2539d7aba56cb2b6569d
Nasrul Hazim <nasrulhazim.m@gmail.com>
2018-08-12 22:51:14 +0800

    Added more common assert

--- src/stubs/tests/TestCase.php
+++ src/stubs/tests/TestCase.php
@@ -2,6 +2,8 @@
 
 namespace DummyNamespace\Tests;
 
+use Illuminate\Support\Facades\Schema;
+
 class TestCase extends \Orchestra\Testbench\TestCase
 {
     /**
@@ -67,5 +69,55 @@ class TestCase extends \Orchestra\Testbench\TestCase
         collect($columns)->each(function ($column) use ($table) {
             $this->assertTrue(Schema::hasColumn($table, $column));
         });
+    }
+
+    /**
+     * Assert has helper
+     *
+     * @param string $helper   helper name
+     */
+    protected function assertHasHelper($helper)
+    {
+        $this->assertTrue(function_exists($helper));
+    }
+
+    /**
+     * Assert has config
+     *
+     * @param string $config   config name
+     */
+    protected function assertHasConfig($config)
+    {
+        $this->assertFileExists(config_path($config));
+    }
+
+    /**
+     * Assert has class
+     *
+     * @param string $class   class name
+     */
+    protected function assertHasClass($class)
+    {
+        $this->assertTrue(class_exists($class));
+    }
+
+    /**
+     * Assert has class method exist
+     *
+     * @param string $object   object 
+     * @param string $method   method 
+     */
+    protected function assertHasClassMethod($object, $method)
+    {
+        $this->assertTrue(method_exists($object, $method));
+    }
+
+    protected function tearDown()
+    {
+        collect(glob(database_path('migrations/*.php')))
+            ->each(function ($path) {
+                unlink($path);
+            });
+        parent::tearDown();
     }
 }
